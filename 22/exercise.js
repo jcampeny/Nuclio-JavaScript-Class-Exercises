b3fc0864dfab2ff592bcb4a74a618f25de049cfe
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 00:52:31 +0200

    Fix Exercise 22 JS file typo errors

--- 22/exercise.js
+++ 22/exercise.js
@@ -1,9 +1,9 @@
 const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronica'];
 
-// 👀 Edita la variable 'result' para comprobar si algún elemento del array tiene más de 7 caracteres
+// 👀 Edita la variable 'result' para comprobar si algún elemento del array tiene más de 7 carácteres
 // 📝
 
-const result = false; // Modificar para usar some y devolver true si algún elemento tiene más de 7 caracteres
+const result = false; // Modificar para usar some y devolver true si algún elemento tiene más de 7 carácteres
 
 // Do not edit below this line
 module.exports = result;
