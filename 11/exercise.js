35f9d19efdff64d4bb76d5e7c0ced7be12a2810c
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 00:05:58 +0200

    Move comment above from Exercise 11 .js file to give the same format as in other exercises

--- 11/exercise.js
+++ 11/exercise.js
@@ -3,7 +3,8 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para que contenga una cadena de texto con todos los nombres de los estudiantes con 💖 entre cada nombre.
 // 📝
 
-const result = ''; // Aquí los estudiantes deben modificar la variable para que una los nombres con '💖' entre ellos
+// Aquí los estudiantes deben modificar la variable para que una los nombres con '💖' entre ellos
+const result = '';
 
 // Do not edit below this line
 module.exports = result;
