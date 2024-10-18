9b18081b723b9089386fee6e5ae953217d14a2ba
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 00:51:10 +0200

    Fix Exercise 21 JS file typo errors

--- 21/exercise.js
+++ 21/exercise.js
@@ -1,9 +1,9 @@
 const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronica'];
 
-// 👀 Edita la variable 'result' para comprobar si todos los elementos tienen más de 3 caracteres
+// 👀 Edita la variable 'result' para comprobar si todos los elementos tienen más de 3 carácteres
 // 📝
 
-const result = false; // Modificar para usar every y devolver true si todos tienen más de 3 caracteres
+const result = false; // Modificar para usar every y devolver true si todos tienen más de 3 carácteres
 
 // Do not edit below this line
 module.exports = result;
